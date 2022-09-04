a47b38c8ee588b56e91a5bc2f79dfd1f84cf340c
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2022-09-04 13:33:24 -0300

    Fixing a bug for deploy

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 app.use('/public', express.static('public'));
 
 //Use ADMINJS router
-app.use('/admin', adminRouter);
+app.use('/', adminRouter);
 
 //Use JSON for API
 app.use(express.json());
